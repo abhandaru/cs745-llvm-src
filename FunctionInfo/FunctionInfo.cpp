a624535393974c6a0534ccee3833e0f7342258e9
Adu Bhandaru <syphondu@gmail.com>
2014-01-28 22:03:17 -0500

    finished function info

--- FunctionInfo/FunctionInfo.cpp
+++ FunctionInfo/FunctionInfo.cpp
@@ -6,6 +6,7 @@
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/Module.h"
+#include "llvm/IR/Instructions.h"
 
 #include <ostream>
 #include <fstream>
@@ -20,9 +21,64 @@ namespace {
 
 class FunctionInfo : public ModulePass {
 
+//
+// Private helper functions
+//
+
+ private:
+  void getFunctionInfo(Module& module, Function& function) {
+    // useful information
+    iplist<BasicBlock>& blocks = function.getBasicBlockList();
+
+    // determine all quantities we need.
+    bool is_var_arg = function.isVarArg();
+    size_t arg_count = function.arg_size();
+    size_t callsite_count = getCallCount(module, function);
+    size_t block_count = blocks.size();
+    size_t instruction_count = 0;
+    for (iplist<BasicBlock>::iterator it = blocks.begin(); it != blocks.end(); ++it) {
+      BasicBlock& block = *it;
+      instruction_count += block.getInstList().size();
+    }
+
+    // output in specified format
+    cout << function.getName().data() << ",\t";
+    if (is_var_arg) {
+      cout << "*,\t";
+    } else {
+      cout << arg_count << ",\t";
+    }
+    cout << callsite_count << ",\t";
+    cout << block_count << ",\t";
+    cout << instruction_count << endl;
+  }
+
+  size_t getCallCount(Module& module, Function& function) {
+    size_t count = 0;
+    for (Module::iterator fn = module.begin(); fn != module.end(); ++fn) {
+      iplist<BasicBlock>& blocks = (*fn).getBasicBlockList();
+
+      for (iplist<BasicBlock>::iterator it = blocks.begin(); it != blocks.end(); ++it) {
+        iplist<Instruction>& instructions = (*it).getInstList();
+
+        for (iplist<Instruction>::iterator itr = instructions.begin(); itr != instructions.end(); ++itr) {
+          Instruction* instr = &(*itr);
+          if (CallInst* call = dyn_cast<CallInst>(instr)) {
+            if (call->getCalledFunction() == &function)
+              count++;
+          }
+        }
+      }
+    }
+
+    // clean up
+    return count;
+  }
+
   // Output the function information to standard out.
   // This function name makes no sense.
   void printFunctionInfo(Module& M) { }
+
 
 public:
 
@@ -44,32 +100,6 @@ public:
   //
   virtual bool runOnFunction(Function &F) {
 
-    // useful information
-    iplist<BasicBlock>& blocks = F.getBasicBlockList();
-
-    // determine all quantities we need.
-    bool is_var_arg = F.isVarArg();
-    size_t arg_count = F.arg_size();
-    size_t callsite_count = 0;
-    size_t block_count = blocks.size();
-    size_t instruction_count = 0;
-    for (iplist<BasicBlock>::iterator it = blocks.begin(); it != blocks.end(); ++it) {
-      BasicBlock& block = *it;
-      iplist<Instruction>& instList = block.getInstList();
-      instruction_count += instList.size();
-    }
-
-    // output in specified format
-    cout << F.getName().data() << ",\t";
-    if (is_var_arg) {
-      cout << "*,\t";
-    } else {
-      cout << arg_count << ",\t";
-    }
-    cout << callsite_count << ",\t";
-    cout << block_count << ",\t";
-    cout << instruction_count << endl;
-
     // always return false
     return false;
   }
@@ -80,7 +110,7 @@ public:
 
     // iterate through all functions in the module
     for (Module::iterator MI = M.begin(), ME = M.end(); MI != ME; ++MI) {
-      runOnFunction(*MI);
+      getFunctionInfo(M, *MI);
     }
 
     // always return false in this example
