825f8068f818ef815a0cdb1c639d907e9b00721f
Adu Bhandaru <syphondu@gmail.com>
2014-01-30 04:29:04 -0500

    removed prints from algebraic optimizer

--- LocalOpts/LocalOpts.cpp
+++ LocalOpts/LocalOpts.cpp
@@ -63,15 +63,12 @@ void strengthReduction(BasicBlock& block) {
 }
 
 void algebraicIdentities(BasicBlock& block) {
-  cout << "  block:" << endl;
   LLVMContext& context = block.getContext();
 
   // iterate through instructions
   for (BasicBlock::iterator it = block.begin(); it != block.end(); ++it) {
     Instruction* instr = &(*it);
-    cout << "    " << instr->getOpcodeName();
     if (BinaryOperator* binOp = dyn_cast<BinaryOperator>(instr)) {
-      cout << " <binary>";
       BinaryOperator::BinaryOps opcode = binOp->getOpcode();
       Value* left = binOp->getOperand(0);
       Value* right = binOp->getOperand(1);
@@ -84,15 +81,11 @@ void algebraicIdentities(BasicBlock& block) {
 
       // both sources are instructions
       if (leftInstr && rightInstr) {
-        cout <<  " <both instr>";
         if (leftInstr->isSameOperationAs(rightInstr)) {
-          cout << " <same>";
           if (opcode == Instruction::Sub) {
-            cout << " <subtract> -> 0";
             ConstantInt* value = ConstantInt::get(Type::getInt32Ty(context), 0);
             ReplaceInstWithValue(block.getInstList(), it, value);
           } else if (opcode == Instruction::SDiv) {
-            cout << " <divide> -> 1";
             // TODO: does not catch divide by zero
             ConstantInt* value = ConstantInt::get(Type::getInt32Ty(context), 1);
             ReplaceInstWithValue(block.getInstList(), it, value);
@@ -107,31 +100,24 @@ void algebraicIdentities(BasicBlock& block) {
 
       // left source is a constant
       else if (leftInstr && rightValue) {
-        cout << " <instr-const>";
         if ((opcode == Instruction::Mul || opcode == Instruction::SDiv) &&
              rightValue->isOne()) {
-          cout << " <instr x/ 1> -> left";
           ReplaceInstWithValue(block.getInstList(), it, leftInstr);
         } else if ((opcode == Instruction::Add || opcode == Instruction::Sub) &&
                     rightValue->isZero()) {
-          cout << " <instr +- 0> -> left";
           ReplaceInstWithValue(block.getInstList(), it, leftInstr);
         }
       }
 
       // left source is a constant
       else if (leftValue && rightInstr) {
-        cout << " <instr-const>";
         if (opcode == Instruction::Mul && leftValue->isOne()) {
-          cout << " <1 x instr> -> right";
           ReplaceInstWithValue(block.getInstList(), it, rightInstr);
         } else if (opcode == Instruction::Add && leftValue->isZero()) {
-          cout << " <0 + instr> -> right";
           ReplaceInstWithValue(block.getInstList(), it, rightInstr);
         }
       }
     }
-    cout << endl;
   }
 }
 
